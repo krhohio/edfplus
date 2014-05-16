79b4e7d7fce81be108b09ca66c978e0d8e1dafe8
krhohio <private@email.com>
2014-05-16 12:32:53 -0400

    Don't forget main!

---
+++ main-edf.cpp
@@ -0,0 +1,62 @@
+// edfplus.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include "edfplus.h"
+
+int main(int argc, char* argv[])
+{
+	int iRetVal = 1;			// be pessimistic
+	
+	CReadEDF *poEDF = NULL;
+	CReadEDF::edfStatus_E eEdfStatus = CReadEDF::EDF_VOID;
+
+    // Fake for loop for common error exit:
+    for( bool allDone = false; allDone == false; allDone = true )
+    {
+		CReadEDF::edfStatus_E eEdfStatus;
+		
+		if( argc != 2 )
+		{
+			cout << endl << "Please enter an input EDF filename as a command line argument!" << endl
+				 << "(example: ""ReadEDF c:\\TestFile1.edf"")";
+			break;			// if not executable name + single input file then exit with error status
+		}
+		
+		poEDF = new CReadEDF( argv[1] );
+
+		if( poEDF->m_eEdfStatus != CReadEDF::EDF_SUCCESS )
+		{
+			if( poEDF->m_eEdfStatus == CReadEDF::EDF_FILE_OPEN_ERROR )
+			{
+				cout << "File read error - please check the path and spelling: " << endl
+					 << "(" << argv[1] << ")" << endl;
+			}
+			break;		// exit with error status
+		}
+
+		cout << "Start Time = " << poEDF->pszGetStartTime() << endl;
+		cout << "Start Date = " << poEDF->pszGetStartDate() << endl;
+		cout << "Number of signals = " << poEDF->pszGetNumberSignals() << endl;
+		cout << "Number of data records = " << poEDF->pszGetNumberRecords() << endl;
+		cout << "Duration of a data record = " << poEDF->pszGetDuration() << endl;
+		
+		int iNumberSignals = poEDF->iGetNumberSignals( &eEdfStatus );
+
+		for( int i=0; i < iNumberSignals; i++ )
+		{
+			cout << "Signal " << i+1 << " Name = " << poEDF->pszGetSignalLabel( i ) << endl;
+		}
+
+		break;
+
+	} // for()
+
+	char cKey = 0;
+	cout << endl << "Press any key followed by the Enter key to continue: ";
+	cin >> cKey;
+
+	return(iRetVal);
+
+} // main()
+
